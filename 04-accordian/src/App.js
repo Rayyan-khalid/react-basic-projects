d4a4e4a5f83364842c7686b2872fc953de0da80a
Rayyan-khalid <reyyankhalid535@gmail.com>
2025-07-09 23:23:54 +0500

    Add initial

--- 04-accordian/src/App.js
+++ 04-accordian/src/App.js
@@ -13,8 +13,8 @@ function App() {
         <h3>questions and answers about login</h3>
         <section className='info'>
           {
-            questions.map(()=>{
-              return<SingleQuestion key={questions.id} {...questions}/>
+            questions.map((question)=>{
+              return<SingleQuestion key={question.id} {...question}/>
             })
           }
         </section>
