3cadce5562525c6194b2ff9b238caaef403a4d96
Rayyan-khalid <reyyankhalid535@gmail.com>
2025-07-09 19:48:28 +0500

    04-accordiaon

---
+++ 04-accordian/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+import './App.css';
+
+function App() {
+
+  const [questions,setQuestions] = useState(data);
+
+  return (
+    <main>
+      <div className='container'> 
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {
+            questions.map(()=>{
+              return<SingleQuestion key={questions.id} {...questions}/>
+            })
+          }
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
